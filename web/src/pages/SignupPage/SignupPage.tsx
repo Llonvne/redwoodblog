072bfa2a9100270c3df58097eac6aa1a5113df5c
Llonvne <llonvne@gmail.com>
2025-08-01 13:09:32 +0800

    Disable signup form and update `/signup` route handling
    - Replaced signup form with "Sign up is closed" message.
    - Uncommented `/signup` route in `Routes.tsx`.

--- web/src/pages/SignupPage/SignupPage.tsx
+++ web/src/pages/SignupPage/SignupPage.tsx
@@ -49,76 +49,78 @@ const SignupPage = () => {
     <>
       <Metadata title="Signup" />
 
-      <main className="rw-main">
-        <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">Signup</h2>
-            </header>
+      <h1 className="rw-main">Sign up is closed</h1>
 
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <Label
-                    name="username"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Username
-                  </Label>
-                  <TextField
-                    name="username"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    ref={usernameRef}
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Username is required',
-                      },
-                    }}
-                  />
-                  <FieldError name="username" className="rw-field-error" />
+      {/*<main className="rw-main">*/}
+      {/*  <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />*/}
+      {/*  <div className="rw-scaffold rw-login-container">*/}
+      {/*    <div className="rw-segment">*/}
+      {/*      <header className="rw-segment-header">*/}
+      {/*        <h2 className="rw-heading rw-heading-secondary">Signup</h2>*/}
+      {/*      </header>*/}
 
-                  <Label
-                    name="password"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Password
-                  </Label>
-                  <PasswordField
-                    name="password"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Password is required',
-                      },
-                    }}
-                  />
-                  <FieldError name="password" className="rw-field-error" />
+      {/*      <div className="rw-segment-main">*/}
+      {/*        <div className="rw-form-wrapper">*/}
+      {/*          <Form onSubmit={onSubmit} className="rw-form-wrapper">*/}
+      {/*            <Label*/}
+      {/*              name="username"*/}
+      {/*              className="rw-label"*/}
+      {/*              errorClassName="rw-label rw-label-error"*/}
+      {/*            >*/}
+      {/*              Username*/}
+      {/*            </Label>*/}
+      {/*            <TextField*/}
+      {/*              name="username"*/}
+      {/*              className="rw-input"*/}
+      {/*              errorClassName="rw-input rw-input-error"*/}
+      {/*              ref={usernameRef}*/}
+      {/*              validation={{*/}
+      {/*                required: {*/}
+      {/*                  value: true,*/}
+      {/*                  message: 'Username is required',*/}
+      {/*                },*/}
+      {/*              }}*/}
+      {/*            />*/}
+      {/*            <FieldError name="username" className="rw-field-error" />*/}
 
-                  <div className="rw-button-group">
-                    <Submit className="rw-button rw-button-blue">
-                      Sign Up
-                    </Submit>
-                  </div>
-                </Form>
-              </div>
-            </div>
-          </div>
-          <div className="rw-login-link">
-            <span>Already have an account?</span>{' '}
-            <Link to={routes.login()} className="rw-link">
-              Log in!
-            </Link>
-          </div>
-        </div>
-      </main>
+      {/*            <Label*/}
+      {/*              name="password"*/}
+      {/*              className="rw-label"*/}
+      {/*              errorClassName="rw-label rw-label-error"*/}
+      {/*            >*/}
+      {/*              Password*/}
+      {/*            </Label>*/}
+      {/*            <PasswordField*/}
+      {/*              name="password"*/}
+      {/*              className="rw-input"*/}
+      {/*              errorClassName="rw-input rw-input-error"*/}
+      {/*              autoComplete="current-password"*/}
+      {/*              validation={{*/}
+      {/*                required: {*/}
+      {/*                  value: true,*/}
+      {/*                  message: 'Password is required',*/}
+      {/*                },*/}
+      {/*              }}*/}
+      {/*            />*/}
+      {/*            <FieldError name="password" className="rw-field-error" />*/}
+
+      {/*            <div className="rw-button-group">*/}
+      {/*              <Submit className="rw-button rw-button-blue">*/}
+      {/*                Sign Up*/}
+      {/*              </Submit>*/}
+      {/*            </div>*/}
+      {/*          </Form>*/}
+      {/*        </div>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*    <div className="rw-login-link">*/}
+      {/*      <span>Already have an account?</span>{' '}*/}
+      {/*      <Link to={routes.login()} className="rw-link">*/}
+      {/*        Log in!*/}
+      {/*      </Link>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</main>*/}
     </>
   )
 }
